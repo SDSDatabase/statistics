8feb88a23df4cac95ab7c5e4d0667685839adad5
Afwan Rezal <afwan.21b6027@gmail.com>
2023-04-18 09:56:08 +0800

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,14 +1,27 @@
 <?php 
-    // Author: Afwan 21B6027
-    // ! To connect to the database
-    $db_server = "localhost";
-    $db_username = "root";
-    $db_pw = "";
-    $db_name = "";
+$db_server = "localhost";
+$db_username = "root";
+$db_pw = "";
+$db_name = "myStats";
 
-    $conn = mysqli_connect($db_server, $db_username, $db_pw, $db_name);
+$conn = mysqli_connect($db_server, $db_username, $db_pw);
+if(!$conn){
+    die('Connection failed: '.mysqli_connect_error());
+}
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+// ! Statements
+$sql_create_db = "CREATE DATABASE $db_name";
+$result = $conn ->query("SHOW DATABASES LIKE  '$db_name'");
+
+if($result->num_rows == 1){
+        echo "Database $db_name exists.";
+} else {
+    if(mysqli_query($conn, $sql_create_db)){
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: ".mysqli_error($conn);
     }
+}
+
+mysqli_close($conn);
 ?>

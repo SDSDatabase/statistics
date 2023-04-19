455574e825c17cd307ead55311e8af08389dfc21
Afwan Rezal <afwan.21b6027@gmail.com>
2023-04-19 21:49:00 +0800

    Update create-form.js

--- js/create-form.js
+++ js/create-form.js
@@ -1,20 +1,34 @@
 import {database, set, get} from "./firebase.js";
 
 // TODO: Create a function which checks available ID from database and updates it accordingly
-var uniqueID; // ! ID used to separate different questions 
-
-// ! References to the HTML page
-    // ! HTML input field(s)
-var formLabel = document.querySelector("#form-label");
-var inputType = document.querySelector("#input-type");
-/*(8-9) Value is the question + the type of value it takes in*/
-
+// var uniqueID = 0; // ! ID used to separate different questions
+var inputCounter = 0;
     // ! Buttons
 var addBtn = document.querySelector("#add-field");
 
 
 function AddField() {
-    
+    // TODO: Create variables that takes the value
+    let formLabel = document.querySelector("#form-label");
+    let inputType = document.querySelector("#input-type");
+
+    const inputAttribute = `input-${inputCounter}`;
+    inputCounter++;
+
+    const label = document.createElement('label');
+    label.textContent = formLabel;
+    label.setAttribute('for', inputAttribute);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", inputType);
+    input.setAttribute("id", inputAttribute);
+    input.setAttribute("name", inputAttribute);
+}
+
+// ? Not sure if this is needed; Will keep it for time being
+function AddToBody(content){
+    const bodyElement = document.querySelector('body');
+    bodyElement.innerHTML += content;
 }
 
 addBtn.addEventListener('click', AddField);

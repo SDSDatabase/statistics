4f89fb61ea1e5ef56233b78a1f1cd730b213fa24
Afwan Rezal <afwan.21b6027@gmail.com>
2023-04-22 20:36:26 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
-import {
-  getDatabase,
-  set,
-  get,
-  update,
-  remove,
-  ref,
-  child
-} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
+import { getDatabase, set, get, update, remove, ref, child } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,6 +17,9 @@ appId: "1:168068170143:web:6676606706ba6b40f67daa"
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase();
 
-export{database, set, get, update, remove, ref, child}
+// ! Realtime Database
+const firebaseDatabase = getDatabase();
+export{firebaseDatabase, set, get, update, remove, ref, child}
+
+
